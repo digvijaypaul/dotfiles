dfc5d0ebc08efcdb979222354963c1766db6df42
Digvijay Paul <digvijaypaul@gmail.com>
2021-02-15 17:21:10 -0600

    Add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+abbr --add v 'nvim'
+abbr --add copm 'config push origin master'
+abbr --add cc 'config checkout'
+abbr --add cs 'config status'
+abbr --add ca 'config add'
+abbr --add ccm 'config commit -m "'
